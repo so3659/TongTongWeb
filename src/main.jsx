fd64e84d155acef3b2f592ae781ee82de6e8a14c
so3659 <so3659@naver.com>
2026-02-04 14:42:20 +0900

    📈 Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.jsx'
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <BlockProvider>
         <App />
+        <Analytics />
       </BlockProvider>
     </AuthProvider>
   </React.StrictMode>,
